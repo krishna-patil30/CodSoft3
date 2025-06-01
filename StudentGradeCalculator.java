7a471bc93b6e3e6029a2d702b3f6b2b50a08cd8f
KRISHNA PATIL <krishnapatil30.2003@gmail.com>
2025-06-01 12:54:18 +0530

    Add files via upload

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static String calculateGrade(float percentage) {
+        if (percentage >= 90) {
+            return "A+";
+        } else if (percentage >= 80) {
+            return "A";
+        } else if (percentage >= 70) {
+            return "B";
+        } else if (percentage >= 60) {
+            return "C";
+        } else if (percentage >= 50) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        float[] marks = new float[numSubjects];
+        float totalMarks = 0;
+
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + " (out of 100): ");
+            marks[i] = scanner.nextFloat();
+            totalMarks += marks[i];
+        }
+
+        float average = totalMarks / numSubjects;
+        String grade = calculateGrade(average);
+
+        System.out.println("\n--- Results ---");
+        System.out.println("Total Marks: " + totalMarks + "/" + (numSubjects * 100));
+        System.out.printf("Average Percentage: %.2f%%\n", average);
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
